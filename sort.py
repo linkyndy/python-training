3de761477b1415fe9d225cca57c6e7a2837fa663
linkyndy <linkyndy@gmail.com>
2013-08-26 14:49:04 +0300

    Further implementation in the sort function

--- sort.py
+++ sort.py
@@ -1,10 +1,45 @@
-def to_dictionary(file):
-	"""Converts a file with a specified format to a dictionary"""
+from collections import OrderedDict
+
+def to_dictionaries(file):
+	"""Converts a file with a specified format to dictionaries"""
 	
+	# Holds the list of dictionaries which will be returned
+	dicts = []
+
+	# Holds the currently parsed dictionary
 	d = {}
 
 	for line in open(file):
-		key, val = line.split()
-		d[key] = val
+		# Start a new dictionary after each empty line; else
+		# append key-value pair to current dictionary
+		if line == '\n':
+			dicts.append(d)
+			d = {}
+		else:
+			key, val = line.split()
+			d[key] = val
 
-	return d
+	dicts.append(d)
+
+	return dicts
+
+def compare(a, b):
+	"""Compares dictionary values"""
+
+	av = a.values()
+	bv = b.values()
+
+	for i in range(max(len(av), len(bv))):
+		return cmp(av[i], bv[i])
+
+def sort_dicts(dict_list):
+	"""Sorts a dictionary list by a specified algorithm"""
+
+	# Make each dict an OrderedDict and sort the keys ascending
+	for i in range(len(dict_list)):
+		dict_list[i] = OrderedDict(sorted(dict_list[i].items(), key=lambda k: k[0]))
+
+	# Sort and return the dict list
+	return sorted(dict_list, cmp=compare)
+
+print sort_dicts(to_dictionaries("dicts.txt"))
