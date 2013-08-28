e8a07d2122c0be8869e1fd8a8019bd61fd2a4474
linkyndy <linkyndy@gmail.com>
2013-08-28 14:52:37 +0300

    Added tests for sort function

---
+++ sort_dictionaries/tests.py
@@ -0,0 +1,25 @@
+from sort_dictionaries import sort_dictionaries as f
+import unittest
+
+class SortDictionariesTests(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def testOne(self):
+		pass
+
+	def testTwo(self):
+		pass
+
+	def testThree(self):
+		pass
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
