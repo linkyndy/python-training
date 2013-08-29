305583282e60f836770b2390ebefcfd80aa5f184
linkyndy <linkyndy@gmail.com>
2013-08-29 13:56:59 +0300

    Updated tests for sort function
    Tests now work properly with files being created and deleted on the fly;
    Added an extra test function.

--- sort_dictionaries/tests.py
+++ sort_dictionaries/tests.py
@@ -1,33 +1,73 @@
 from sort_dictionaries import sort_dictionaries as f
 import unittest
-from tempfile import NamedTemporaryFile
+import os
 
 class SortDictionariesTests(unittest.TestCase):
 	"""Various inputs for the dictionary sorting function"""
 
 	def testOne(self):
 		data = 'a 1\nb 2\n\nb 0\na 9'
-		expected = '1 2'
-		with NamedTemporaryFile() as infile, NamedTemporaryFile() as outfile:
-			infile.write(data)
-			f(infile.name, outfile.name)
-			self.assertEqual(outfile.read(), expected)
+		expected = '1 2 '
+
+		g = open('in.txt', 'w')
+		g.write(data)
+		g.close()
+
+		f('in.txt', 'out.txt')
+
+		h = open('out.txt')
+		self.assertEqual(h.read(), expected)
+
+		os.remove('in.txt')
+		os.remove('out.txt')
 
 	def testTwo(self):
 		data = 'b 1\nc 2\n\nb 0\na 9'
-		expected = '1 2'
-		with NamedTemporaryFile() as infile, NamedTemporaryFile() as outfile:
-			infile.write(data)
-			f(infile.name, outfile.name)
-			self.assertEqual(outfile.read(), expected)
+		expected = '1 2 '
+
+		g = open('in.txt', 'w')
+		g.write(data)
+		g.close()
+
+		f('in.txt', 'out.txt')
+
+		h = open('out.txt')
+		self.assertEqual(h.read(), expected)
+
+		os.remove('in.txt')
+		os.remove('out.txt')
 
 	def testThree(self):
 		data = 'b 1\nc 2\n\nb 1\nc 2'
-		expected = '1 2'
-		with NamedTemporaryFile() as infile, NamedTemporaryFile() as outfile:
-			infile.write(data)
-			f(infile.name, outfile.name)
-			self.assertEqual(outfile.read(), expected)
+		expected = '2 1 '
+
+		g = open('in.txt', 'w')
+		g.write(data)
+		g.close()
+
+		f('in.txt', 'out.txt')
+
+		h = open('out.txt')
+		self.assertEqual(h.read(), expected)
+
+		os.remove('in.txt')
+		os.remove('out.txt')
+
+	def testFour(self):
+		data = 'b 1\nc 2'
+		expected = '1 '
+
+		g = open('in.txt', 'w')
+		g.write(data)
+		g.close()
+
+		f('in.txt', 'out.txt')
+
+		h = open('out.txt')
+		self.assertEqual(h.read(), expected)
+
+		os.remove('in.txt')
+		os.remove('out.txt')
 
 def main():
 	unittest.main()
