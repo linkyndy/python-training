d8ec0dac442b3388ed15f051c0c8821e3b3fa1bf
linkyndy <linkyndy@gmail.com>
2013-09-11 13:55:34 +0300

    Implemented the card dealer function
    Finished implementing the card dealer function. It builds a game consisting of a dealer and a number of players, deals cards to the players and also deals the cards on the table.

--- card_dealer/card_dealer.py
+++ card_dealer/card_dealer.py
@@ -1,30 +1,53 @@
+import random
+
 class Card(object):
 	def __init__(self, code):
 		self.code = code
 
 	def __str__(self):
-		codes = {0: '2', 1: '3', 12: 'A'}
+		codes = ('2','3','4','5','6','7','8','9','10','J','Q','K','A')
+		symbols = (u'\u2660', u'\u2663', u'\u2665', u'\u2666')
 
-		return codes[self.code % 13] + ' ' + self.code / 13
+		return (codes[self.code%13] + symbols[self.code/13]).encode('utf-8')
 
 class Deck(object):
 	def __init__(self):
-		self.cards = [Card(i) for i in range(1, )]
+		self.cards = [Card(i) for i in range(0, 52)]
+
+	def __str__(self):
+		return '  '.join([str(card) for card in self.cards])
+
+	def shuffle(self):
+		random.shuffle(self.cards)
+
+	def sort(self):
+		self.cards.sort()
 
 	def draw(self):
-		pass
+		return self.cards.pop()
 
 class Hand(object):
-	def __init__(self):
-		pass
+	def __init__(self, deck):
+		self.cards = [deck.draw(), deck.draw()]
+
+	def __str__(self):
+		return '  '.join([str(card) for card in self.cards])
 
 class Player(object):
 	def __init__(self, name):
 		self.name = name
 
-class Dealer(Player):
-	def deal(self, players):
-		self.players = players
+	def __str__(self):
+		return self.name
 
-if __name__ == '__main__':
-	pass
+class Dealer(Player):
+	def deal(self, players, times=1):
+		deck = Deck()
+		game = {}
+
+		for time in range(0, times):
+			deck.shuffle()
+			game['players'] = [{p: Hand(deck)} for p in players]
+			game['table'] = [deck.draw() for i in range(0, 5)]
+
+		return game
