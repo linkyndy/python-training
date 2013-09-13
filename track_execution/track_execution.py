99f8411adbcea7686589ddbfdce9fbb5834fbe2a
linkyndy <linkyndy@gmail.com>
2013-09-13 10:59:21 +0300

    Implemented track execution decorator
    Finished implementing the track execution decorator with an optional threshold parameter.

--- track_execution/track_execution.py
+++ track_execution/track_execution.py
@@ -1,26 +1,14 @@
 import time
-import inspect
 
-
-def timer(func, threshold=0):
-	def inner(*args, **kwargs):
-		start = time.clock()
-		ret = func(*args, **kwargs)
-		end = time.clock()
-		print 'Function ', inspect.stack()[1][3], ' took ', (end - start), 's'
-		return ret
-	return inner
-
-@timer
-def function():
-	pass
-
-@timer
-def longfunction():
-	a = []
-	for i in range(0,10000000):
-		a.append(i)
-
-if __name__ == '__main__':
-	function()
-	longfunction()
+def timer(threshold=9999):
+	def wrap(func):
+		def inner(*args, **kwargs):
+			start = time.time()
+			ret = func(*args, **kwargs)
+			end = time.time()
+			print 'Function {name} took {time:.4f}s ({thres} ' \
+				  'threshold)'.format(name=func.__name__, time=end-start, \
+				  thres='within' if (end-start) < threshold else 'not in')
+			return ret
+		return inner
+	return wrap
