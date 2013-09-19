a65c40f2a3a33bce04c42a8c9dddf792732d8e7b
linkyndy <linkyndy@gmail.com>
2013-09-19 16:00:01 +0300

    Refactored code for track execution decorator

--- track_execution/track_execution.py
+++ track_execution/track_execution.py
@@ -1,14 +1,18 @@
 import time
 
+output = 'Function {name} took {time:.4f}s ({thres} threshold)'
+
+
 def timer(threshold=9999):
-	def wrap(func):
-		def inner(*args, **kwargs):
-			start = time.time()
-			ret = func(*args, **kwargs)
-			end = time.time()
-			print 'Function {name} took {time:.4f}s ({thres} ' \
-				  'threshold)'.format(name=func.__name__, time=end-start, \
-				  thres='within' if (end-start) < threshold else 'not in')
-			return ret
-		return inner
-	return wrap
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            start = time.time()
+            ret = func(*args, **kwargs)
+            end = time.time()
+            print output.format(
+                name=func.__name__,
+                time=end-start,
+                thres='within' if (end-start) < threshold else 'not in')
+            return ret
+        return inner
+    return wrapper
