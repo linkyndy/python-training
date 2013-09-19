50ccbce2cf2a82217065a2b6755911ab8bdb1d99
linkyndy <linkyndy@gmail.com>
2013-09-19 14:54:23 +0300

    Refactored subset generator code

--- subset_generator/subset_generator.py
+++ subset_generator/subset_generator.py
@@ -1,10 +1,11 @@
-from itertools import chain, combinations, imap
+from itertools import chain, combinations
+
 
 def subset_generator(s):
-	"""Generates powerset of given set
-	set([1, 2]) => [set([]), set([1]), set([2]), set([1, 2])]
-	"""
+    """Generates powerset of given set
+    set([1, 2]) => [set([]), set([1]), set([2]), set([1, 2])]
+    """
 
-	subsets = chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-	for subset in subsets:
-		yield set(subset)
+    subsets = chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    for subset in subsets:
+        yield set(subset)
--- subset_generator/tests.py
+++ subset_generator/tests.py
@@ -1,35 +1,37 @@
-from subset_generator import subset_generator as f
+from subset_generator import subset_generator
 import unittest
 
+
 class GeneratorTests(unittest.TestCase):
-	"""Various generator tests"""
+    """Various generator tests"""
 
-	def testEmptySet(self):
-		s = set()
-		expected = [set()]
+    def testEmptySet(self):
+        s = set()
+        expected = [set()]
 
-		self.assertEqual([e for e in f(s)], expected)
+        self.assertEqual([e for e in subset_generator(s)], expected)
 
-	def testAnotherEmptySet(self):
-		s = set([])
-		expected = [set([])]
+    def testAnotherEmptySet(self):
+        s = set([])
+        expected = [set([])]
 
-		self.assertEqual([e for e in f(s)], expected)
+        self.assertEqual([e for e in subset_generator(s)], expected)
 
-	def testOneElementSet(self):
-		s = set([1])
-		expected = [set([]), set([1])]
+    def testOneElementSet(self):
+        s = set([1])
+        expected = [set([]), set([1])]
 
-		self.assertEqual([e for e in f(s)], expected)
+        self.assertEqual([e for e in subset_generator(s)], expected)
 
-	def testTwoElementSet(self):
-		s = set([1, 2])
-		expected = [set([]), set([1]), set([2]), set([1, 2])]
+    def testTwoElementSet(self):
+        s = set([1, 2])
+        expected = [set([]), set([1]), set([2]), set([1, 2])]
 
-		self.assertEqual([e for e in f(s)], expected)
+        self.assertEqual([e for e in subset_generator(s)], expected)
+
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
