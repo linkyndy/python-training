d6359198429dfceab8244145c8265d5ce03462d8
linkyndy <linkyndy@gmail.com>
2013-09-19 16:09:21 +0300

    Refactored code for merge two objects function

--- merge_two_objects/merge_two_objects.py
+++ merge_two_objects/merge_two_objects.py
@@ -1,19 +1,19 @@
 def merge_two_objects(a, b):
-	"""Merge two objects up to any depth"""
+    """Merge two objects up to any depth"""
 
-	while len(a) > 0:
-		key, value = a.popitem()
+    while len(a) > 0:
+        key, value = a.popitem()
 
-		if key in b:
-			if type(value) != type(b[key]):
-				b[key] = (value, b[key])
-			elif isinstance(value, dict):
-				b[key] = merge_two_objects(value, b[key])
-			elif isinstance(value, set):
-				b[key] = value.union(b[key])
-			else:
-				b[key] = value + b[key]
-		else:
-			b[key] = value
+        if key in b:
+            if type(value) != type(b[key]):
+                b[key] = (value, b[key])
+            elif isinstance(value, dict):
+                b[key] = merge_two_objects(value, b[key])
+            elif isinstance(value, set):
+                b[key] = value.union(b[key])
+            else:
+                b[key] = value + b[key]
+        else:
+            b[key] = value
 
-	return b
+    return b
--- merge_two_objects/tests.py
+++ merge_two_objects/tests.py
@@ -1,69 +1,71 @@
-from merge_two_objects import merge_two_objects as f
+from merge_two_objects import merge_two_objects
 import unittest
+
 
 class MergeTwoObjectsTests(unittest.TestCase):
 
-	def testEmptyObjects(self):
-		a = b = {}
-		expected = {}
-		self.assertEqual(f(a, b), expected)
+    def testEmptyObjects(self):
+        a = b = {}
+        expected = {}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testIntObjects(self):
-		a = {'key': 1}
-		b = {'key': 2}
-		expected = {'key': 3}
-		self.assertEqual(f(a, b), expected)
+    def testIntObjects(self):
+        a = {'key': 1}
+        b = {'key': 2}
+        expected = {'key': 3}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testFloatObjects(self):
-		a = {'key': 1.0}
-		b = {'key': 2.0}
-		expected = {'key': 3.0}
-		self.assertEqual(f(a, b), expected)
+    def testFloatObjects(self):
+        a = {'key': 1.0}
+        b = {'key': 2.0}
+        expected = {'key': 3.0}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testStringObjects(self):
-		a = {'key': 'abc'}
-		b = {'key': 'def'}
-		expected = {'key': 'abcdef'}
-		self.assertEqual(f(a, b), expected)
+    def testStringObjects(self):
+        a = {'key': 'abc'}
+        b = {'key': 'def'}
+        expected = {'key': 'abcdef'}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testTupleObjects(self):
-		a = {'key': (1, 2)}
-		b = {'key': (3, 4)}
-		expected = {'key': (1, 2, 3, 4)}
-		self.assertEqual(f(a, b), expected)
+    def testTupleObjects(self):
+        a = {'key': (1, 2)}
+        b = {'key': (3, 4)}
+        expected = {'key': (1, 2, 3, 4)}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testListObjects(self):
-		a = {'key': [1, 2]}
-		b = {'key': [3, 4]}
-		expected = {'key': [1, 2, 3, 4]}
-		self.assertEqual(f(a, b), expected)
+    def testListObjects(self):
+        a = {'key': [1, 2]}
+        b = {'key': [3, 4]}
+        expected = {'key': [1, 2, 3, 4]}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testSetObjects(self):
-		a = {'key': set([1, 2])}
-		b = {'key': set([3, 4])}
-		expected = {'key': set([1, 2, 3, 4])}
-		self.assertEqual(f(a, b), expected)
+    def testSetObjects(self):
+        a = {'key': set([1, 2])}
+        b = {'key': set([3, 4])}
+        expected = {'key': set([1, 2, 3, 4])}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testDictionaryObjects(self):
-		a = {'key': {'inner_key': 1}}
-		b = {'key': {'inner_key': 2}}
-		expected = {'key': {'inner_key': 3}}
-		self.assertEqual(f(a, b), expected)
+    def testDictionaryObjects(self):
+        a = {'key': {'inner_key': 1}}
+        b = {'key': {'inner_key': 2}}
+        expected = {'key': {'inner_key': 3}}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testMultiDictionaryObjects(self):
-		a = {'key': {'inner_key': 1, 'another_key': 2}}
-		b = {'key': {'inner_key': 2, 'another_key': 3}}
-		expected = {'key': {'inner_key': 3, 'another_key': 5}}
-		self.assertEqual(f(a, b), expected)
+    def testMultiDictionaryObjects(self):
+        a = {'key': {'inner_key': 1, 'another_key': 2}}
+        b = {'key': {'inner_key': 2, 'another_key': 3}}
+        expected = {'key': {'inner_key': 3, 'another_key': 5}}
+        self.assertEqual(merge_two_objects(a, b), expected)
 
-	def testDifferentTypeObjects(self):
-		a = {'key': 1}
-		b = {'key': 'abc'}
-		expected = {'key': (1, 'abc')}
-		self.assertEqual(f(a, b), expected)
+    def testDifferentTypeObjects(self):
+        a = {'key': 1}
+        b = {'key': 'abc'}
+        expected = {'key': (1, 'abc')}
+        self.assertEqual(merge_two_objects(a, b), expected)
+
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
