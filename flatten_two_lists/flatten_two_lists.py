8a5792ea0b41b3913032dbbb670b4a84d6d8401f
linkyndy <linkyndy@gmail.com>
2013-09-19 16:07:22 +0300

    Refactored code for flatten two lists function

--- flatten_two_lists/flatten_two_lists.py
+++ flatten_two_lists/flatten_two_lists.py
@@ -1,18 +1,18 @@
 def flatten_two_lists(a, b, depth):
-	"""Flatten two lists up to depth"""
+    """Flatten two lists up to depth"""
 
-	def flatten(l, depth):
-		"""Recursively flatten list up to depth"""
+    def flatten(l, depth):
+        """Recursively flatten list up to depth"""
 
-		if isinstance(l, list):
-			if depth <= 0:
-				return l
-			if len(l) == 0:
-				return []
-			
-			first, rest = l[0], l[1:]
-			return flatten(first, depth - 1) + flatten(rest, depth)
-		else:
-			return [l]
+        if isinstance(l, list):
+            if depth <= 0:
+                return l
+            if len(l) == 0:
+                return []
 
-	return flatten(a, depth), flatten(b, depth)
+            first, rest = l[0], l[1:]
+            return flatten(first, depth - 1) + flatten(rest, depth)
+        else:
+            return [l]
+
+    return flatten(a, depth), flatten(b, depth)
--- flatten_two_lists/tests.py
+++ flatten_two_lists/tests.py
@@ -1,184 +1,190 @@
-from flatten_two_lists import flatten_two_lists as f
+from flatten_two_lists import flatten_two_lists
 import unittest
 
+
 class EmptyListsTests(unittest.TestCase):
-	"""Both lists empty, various depth"""
+    """Both lists empty, various depth"""
 
-	def setUp(self):
-		self.a = self.b = []
+    def setUp(self):
+        self.a = self.b = []
 
-	def testNoDepth(self):
-		expected = ([], [])
-		self.assertEqual(f(self.a, self.b, 0), expected)
+    def testNoDepth(self):
+        expected = ([], [])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 0), expected)
 
-	def testOneDepth(self):
-		expected = ([], [])
-		self.assertEqual(f(self.a, self.b, 1), expected)
+    def testOneDepth(self):
+        expected = ([], [])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 1), expected)
 
-	def testRandomDepth(self):
-		expected = ([], [])
-		self.assertEqual(f(self.a, self.b, 195667), expected)
+    def testRandomDepth(self):
+        expected = ([], [])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 195667), expected)
 
-	def testNegativeDepth(self):
-		expected = ([], [])
-		self.assertEqual(f(self.a, self.b, -345), expected)
+    def testNegativeDepth(self):
+        expected = ([], [])
+        self.assertEqual(flatten_two_lists(self.a, self.b, -345), expected)
+
 
 class SingleElementListsTests(unittest.TestCase):
-	"""Both lists of one element, various depth"""
+    """Both lists of one element, various depth"""
 
-	def setUp(self):
-		self.a = [1]
-		self.b = [2]
+    def setUp(self):
+        self.a = [1]
+        self.b = [2]
 
-	def testNoDepth(self):
-		expected = ([1], [2])
-		self.assertEqual(f(self.a, self.b, 0), expected)
+    def testNoDepth(self):
+        expected = ([1], [2])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 0), expected)
 
-	def testOneDepth(self):
-		expected = ([1], [2])
-		self.assertEqual(f(self.a, self.b, 1), expected)
+    def testOneDepth(self):
+        expected = ([1], [2])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 1), expected)
 
-	def testRandomDepth(self):
-		expected = ([1], [2])
-		self.assertEqual(f(self.a, self.b, 456756), expected)
+    def testRandomDepth(self):
+        expected = ([1], [2])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 456756), expected)
 
-	def testNegativeDepth(self):
-		expected = ([1], [2])
-		self.assertEqual(f(self.a, self.b, -456), expected)
+    def testNegativeDepth(self):
+        expected = ([1], [2])
+        self.assertEqual(flatten_two_lists(self.a, self.b, -456), expected)
+
 
 class MultiElementListsTests(unittest.TestCase):
-	"""Simple lists with more than one element, various depth"""
-	
-	def setUp(self):
-		self.a = [1, 2, 3]
-		self.b = [4, 5]
+    """Simple lists with more than one element, various depth"""
 
-	def testNoDepth(self):
-		expected = ([1, 2, 3], [4, 5])
-		self.assertEqual(f(self.a, self.b, 0), expected)
+    def setUp(self):
+        self.a = [1, 2, 3]
+        self.b = [4, 5]
 
-	def testOneDepth(self):
-		expected = ([1, 2, 3], [4, 5])
-		self.assertEqual(f(self.a, self.b, 1), expected)
+    def testNoDepth(self):
+        expected = ([1, 2, 3], [4, 5])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 0), expected)
 
-	def testRandomDepth(self):
-		expected = ([1, 2, 3], [4, 5])
-		self.assertEqual(f(self.a, self.b, 456565), expected)
+    def testOneDepth(self):
+        expected = ([1, 2, 3], [4, 5])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 1), expected)
 
-	def testNegativeDepth(self):
-		expected = ([1, 2, 3], [4, 5])
-		self.assertEqual(f(self.a, self.b, -346), expected)
+    def testRandomDepth(self):
+        expected = ([1, 2, 3], [4, 5])
+        self.assertEqual(flatten_two_lists(self.a, self.b, 456565), expected)
+
+    def testNegativeDepth(self):
+        expected = ([1, 2, 3], [4, 5])
+        self.assertEqual(flatten_two_lists(self.a, self.b, -346), expected)
+
 
 class CombinedElementListsTests(unittest.TestCase):
-	"""Simple lists with various elements, various depth"""
+    """Simple lists with various elements, various depth"""
 
-	def testNoOneElementsNoDepth(self):
-		a = []
-		b = [1]
-		expected = ([], [1])
-		self.assertEqual(f(a, b, 0), expected)
+    def testNoOneElementsNoDepth(self):
+        a = []
+        b = [1]
+        expected = ([], [1])
+        self.assertEqual(flatten_two_lists(a, b, 0), expected)
 
-	def testNoOneElementsRandomDepth(self):
-		a = []
-		b = [1]
-		expected = ([], [1])
-		self.assertEqual(f(a, b, 456784), expected)
+    def testNoOneElementsRandomDepth(self):
+        a = []
+        b = [1]
+        expected = ([], [1])
+        self.assertEqual(flatten_two_lists(a, b, 456784), expected)
 
-	def testNoOneElementsNegativeDepth(self):
-		a = []
-		b = [1]
-		expected = ([], [1])
-		self.assertEqual(f(a, b, -834), expected)
+    def testNoOneElementsNegativeDepth(self):
+        a = []
+        b = [1]
+        expected = ([], [1])
+        self.assertEqual(flatten_two_lists(a, b, -834), expected)
 
-	def testNoMultiElementsNoDepth(self):
-		a = []
-		b = [1, 2, 3]
-		expected = ([], [1, 2, 3])
-		self.assertEqual(f(a, b, 0), expected)
+    def testNoMultiElementsNoDepth(self):
+        a = []
+        b = [1, 2, 3]
+        expected = ([], [1, 2, 3])
+        self.assertEqual(flatten_two_lists(a, b, 0), expected)
 
-	def testNoMultiElementsRandomDepth(self):
-		a = []
-		b = [1, 2, 3]
-		expected = ([], [1, 2, 3])
-		self.assertEqual(f(a, b, 347658), expected)
+    def testNoMultiElementsRandomDepth(self):
+        a = []
+        b = [1, 2, 3]
+        expected = ([], [1, 2, 3])
+        self.assertEqual(flatten_two_lists(a, b, 347658), expected)
 
-	def testNoMultiElementsNegativeDepth(self):
-		a = []
-		b = [1, 2, 3]
-		expected = ([], [1, 2, 3])
-		self.assertEqual(f(a, b, -589), expected)
+    def testNoMultiElementsNegativeDepth(self):
+        a = []
+        b = [1, 2, 3]
+        expected = ([], [1, 2, 3])
+        self.assertEqual(flatten_two_lists(a, b, -589), expected)
 
-	def testOneMultiElementsNoDepth(self):
-		a = [1]
-		b = [2, 3]
-		expected = ([1], [2, 3])
-		self.assertEqual(f(a, b, 0), expected)
+    def testOneMultiElementsNoDepth(self):
+        a = [1]
+        b = [2, 3]
+        expected = ([1], [2, 3])
+        self.assertEqual(flatten_two_lists(a, b, 0), expected)
 
-	def testOneMultiElementsRandomDepth(self):
-		a = [1]
-		b = [2, 3]
-		expected = ([1], [2, 3])
-		self.assertEqual(f(a, b, 456777), expected)
+    def testOneMultiElementsRandomDepth(self):
+        a = [1]
+        b = [2, 3]
+        expected = ([1], [2, 3])
+        self.assertEqual(flatten_two_lists(a, b, 456777), expected)
 
-	def testOneMultiElementsNegativeDepth(self):
-		a = [1]
-		b = [2, 3]
-		expected = ([1], [2, 3])
-		self.assertEqual(f(a, b, -476), expected)
+    def testOneMultiElementsNegativeDepth(self):
+        a = [1]
+        b = [2, 3]
+        expected = ([1], [2, 3])
+        self.assertEqual(flatten_two_lists(a, b, -476), expected)
+
 
 class LayeredListsTests(unittest.TestCase):
-	"""Layered lists with various elements, various depth"""
+    """Layered lists with various elements, various depth"""
 
-	def testOneLayerNoDepth(self):
-		a = [1, [2], 3]
-		b = [4, [5], 6]
-		expected = ([1, [2], 3], [4, [5], 6])
-		self.assertEqual(f(a, b, 0), expected)
+    def testOneLayerNoDepth(self):
+        a = [1, [2], 3]
+        b = [4, [5], 6]
+        expected = ([1, [2], 3], [4, [5], 6])
+        self.assertEqual(flatten_two_lists(a, b, 0), expected)
 
-	def testOneLayerOneDepth(self):
-		a = [1, [2], 3]
-		b = [4, [5], 6]
-		expected = ([1, 2, 3], [4, 5, 6])
-		self.assertEqual(f(a, b, 1), expected)
+    def testOneLayerOneDepth(self):
+        a = [1, [2], 3]
+        b = [4, [5], 6]
+        expected = ([1, 2, 3], [4, 5, 6])
+        self.assertEqual(flatten_two_lists(a, b, 1), expected)
 
-	def testOneLayerRandomDepth(self):
-		a = [1, [2], 3]
-		b = [4, [5], 6]
-		expected = ([1, 2, 3], [4, 5, 6])
-		self.assertEqual(f(a, b, 157869), expected)
+    def testOneLayerRandomDepth(self):
+        a = [1, [2], 3]
+        b = [4, [5], 6]
+        expected = ([1, 2, 3], [4, 5, 6])
+        self.assertEqual(flatten_two_lists(a, b, 157869), expected)
 
-	def testOneLayerNegativeDepth(self):
-		a = [1, [2], 3]
-		b = [4, [5], 6]
-		expected = ([1, [2], 3], [4, [5], 6])
-		self.assertEqual(f(a, b, -577), expected)
+    def testOneLayerNegativeDepth(self):
+        a = [1, [2], 3]
+        b = [4, [5], 6]
+        expected = ([1, [2], 3], [4, [5], 6])
+        self.assertEqual(flatten_two_lists(a, b, -577), expected)
 
-	def testMultiLayerNoDepth(self):
-		a = [1, [2, [3, 4], [5]], 6, [7]]
-		b = [8, [[9], []], []]
-		expected = ([1, [2, [3, 4], [5]], 6, [7]], [8, [[9], []], []])
-		self.assertEqual(f(a, b, 0), expected)
+    def testMultiLayerNoDepth(self):
+        a = [1, [2, [3, 4], [5]], 6, [7]]
+        b = [8, [[9], []], []]
+        expected = ([1, [2, [3, 4], [5]], 6, [7]], [8, [[9], []], []])
+        self.assertEqual(flatten_two_lists(a, b, 0), expected)
 
-	def testMultiLayerOneDepth(self):
-		a = [1, [2, [3, 4], [5]], 6, [7]]
-		b = [8, [[9], []], []]
-		expected = ([1, 2, [3, 4], [5], 6, 7], [8, [9], []])
-		self.assertEqual(f(a, b, 1), expected)
+    def testMultiLayerOneDepth(self):
+        a = [1, [2, [3, 4], [5]], 6, [7]]
+        b = [8, [[9], []], []]
+        expected = ([1, 2, [3, 4], [5], 6, 7], [8, [9], []])
+        self.assertEqual(flatten_two_lists(a, b, 1), expected)
 
-	def testMultiLayerRandomDepth(self):
-		a = [1, [2, [3, 4], [5]], 6, [7]]
-		b = [8, [[9], []], []]
-		expected = ([1, 2, 3, 4, 5, 6, 7], [8, 9])
-		self.assertEqual(f(a, b, 849633), expected)
+    def testMultiLayerRandomDepth(self):
+        a = [1, [2, [3, 4], [5]], 6, [7]]
+        b = [8, [[9], []], []]
+        expected = ([1, 2, 3, 4, 5, 6, 7], [8, 9])
+        self.assertEqual(flatten_two_lists(a, b, 849633), expected)
 
-	def testMultiLayerNegativeDepth(self):
-		a = [1, [2, [3, 4], [5]], 6, [7]]
-		b = [8, [[9], []], []]
-		expected = ([1, [2, [3, 4], [5]], 6, [7]], [8, [[9], []], []])
-		self.assertEqual(f(a, b, -561), expected)
+    def testMultiLayerNegativeDepth(self):
+        a = [1, [2, [3, 4], [5]], 6, [7]]
+        b = [8, [[9], []], []]
+        expected = ([1, [2, [3, 4], [5]], 6, [7]], [8, [[9], []], []])
+        self.assertEqual(flatten_two_lists(a, b, -561), expected)
+
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
