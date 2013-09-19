faf8cd6846800b377d3186d34f217a9265def2e4
linkyndy <linkyndy@gmail.com>
2013-09-19 15:03:53 +0300

    Refactored code for preorder generator

--- preorder_generator/preorder_generator.py
+++ preorder_generator/preorder_generator.py
@@ -1,13 +1,13 @@
 def preorder_generator(tree):
-	if not tree:
-		return
-		
-	yield tree[0]
+    if not tree:
+        return
 
-	if tree[1] is not None:
-		for ltree in preorder_generator(tree[1]):
-			yield ltree
+    yield tree[0]
 
-	if tree[2] is not None:
-		for rtree in preorder_generator(tree[2]):
-			yield rtree
+    if tree[1]:
+        for ltree in preorder_generator(tree[1]):
+            yield ltree
+
+    if tree[2]:
+        for rtree in preorder_generator(tree[2]):
+            yield rtree
--- preorder_generator/tests.py
+++ preorder_generator/tests.py
@@ -1,53 +1,56 @@
-from preorder_generator import preorder_generator as f
+from preorder_generator import preorder_generator
 import unittest
 
+
 class GeneratorTests(unittest.TestCase):
-	"""Various generator tests"""
+    """Various generator tests"""
 
-	def testEmptyTree(self):
-		tree = ()
-		expected = []
+    def testEmptyTree(self):
+        tree = ()
+        expected = []
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
 
-	def testSingleNodeTree(self):
-		tree = ('a', None, None)
-		expected = ['a']
+    def testSingleNodeTree(self):
+        tree = ('a', None, None)
+        expected = ['a']
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
 
-	def testTwoNodeTree(self):
-		tree = ('a', ('b', None, None), None)
-		expected = ['a', 'b']
+    def testTwoNodeTree(self):
+        tree = ('a', ('b', None, None), None)
+        expected = ['a', 'b']
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
 
-	def testAnotherTwoNodeTree(self):
-		tree = ('a', None, ('b', None, None))
-		expected = ['a', 'b']
+    def testAnotherTwoNodeTree(self):
+        tree = ('a', None, ('b', None, None))
+        expected = ['a', 'b']
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
 
-	def testThreeNodeTree(self):
-		tree = ('a', ('b', None, None), ('c', None, None))
-		expected = ['a', 'b', 'c']
+    def testThreeNodeTree(self):
+        tree = ('a', ('b', None, None), ('c', None, None))
+        expected = ['a', 'b', 'c']
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
 
-	def testMultiNodeTree(self):
-		tree = ('a', ('b', ('c', None, None), None), ('d', None, ('e', None, None)))
-		expected = ['a', 'b', 'c', 'd', 'e']
+    def testMultiNodeTree(self):
+        tree = ('a', ('b', ('c', None, None), None),
+               ('d', None, ('e', None, None)))
+        expected = ['a', 'b', 'c', 'd', 'e']
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
 
-	def testAnotherMultiNodeTree(self):
-		tree = ('a', ('b', ('c', None, None), None), None)
-		expected = ['a', 'b', 'c']
+    def testAnotherMultiNodeTree(self):
+        tree = ('a', ('b', ('c', None, None), None), None)
+        expected = ['a', 'b', 'c']
 
-		self.assertEqual([x for x in f(tree)], expected)
+        self.assertEqual([x for x in preorder_generator(tree)], expected)
+
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
