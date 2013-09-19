74ccb6b0607a188597a1c75d78d215d805f01910
linkyndy <linkyndy@gmail.com>
2013-09-19 16:02:41 +0300

    Refactored code for singleton pattern metaclass

--- singleton_pattern/singleton_pattern.py
+++ singleton_pattern/singleton_pattern.py
@@ -1,12 +1,13 @@
 class Singleton(type):
-	"""Metaclass which implements the singleton pattern"""
+    """Metaclass which implements the singleton pattern"""
 
-	_instances = {}
+    _instances = {}
 
-	def __call__(self, *args, **kwargs):
-		if self not in self._instances:
-			self._instances[self] = super(Singleton, self).__call__(*args, **kwargs)
-		return self._instances[self]
+    def __call__(self, *args, **kwargs):
+        if self not in self._instances:
+            self._instances[self] = super(Singleton, self).__call__(*args,
+                                                                    **kwargs)
+        return self._instances[self]
 
-	def __copy__(self, *args, **kwargs):
-		return self._instances[self]
+    def __copy__(self, *args, **kwargs):
+        return self._instances[self]
