65a4cd83b10b6c8bf8b8e292c2b8c6bd8bf164c5
linkyndy <linkyndy@gmail.com>
2013-09-19 15:46:58 +0300

    Refactored code for swap key-value function

--- swap_key_values/swap_key_values.py
+++ swap_key_values/swap_key_values.py
@@ -1,28 +1,28 @@
 def swap_key_values(d):
-	"""Swap key with values in a dictionary if it is possible"""
+    """Swap key with values in a dictionary if it is possible"""
 
-	def valid_key(key):
-		"""Checks whether the parameter is a valid key"""
+    def valid_key(key):
+        """Checks whether the parameter is a valid key"""
 
-		if (isinstance(key, int) or 
-		   isinstance(key, float) or 
-		   isinstance(key, long) or 
-		   isinstance(key, complex) or
-		   isinstance(key, basestring)):
-			return True
-		elif isinstance(key, tuple):
-			for item in key:
-				if not valid_key(item):
-					return False
-			return True
-		else:
-			return False
+        if isinstance(key, int) or \
+           isinstance(key, float) or \
+           isinstance(key, long) or \
+           isinstance(key, complex) or \
+           isinstance(key, basestring):
+            return True
+        elif isinstance(key, tuple):
+            for item in key:
+                if not valid_key(item):
+                    return False
+            return True
+        else:
+            return False
 
-	e = {}
+    swapped = {}
 
-	for key, value in d.iteritems():
-		if not valid_key(value):
-			return False
-		e[value] = key
+    for key, value in d.iteritems():
+        if not valid_key(value):
+            return False
+        swapped[value] = key
 
-	return e
+    return swapped
--- swap_key_values/tests.py
+++ swap_key_values/tests.py
@@ -1,78 +1,81 @@
-from swap_key_values import swap_key_values as f
+from swap_key_values import swap_key_values
 import unittest
 
+
 class SwapKeyValuesTests(unittest.TestCase):
-	"""Tests for valid dictionary key types"""
+    """Tests for valid dictionary key types"""
 
-	def testInt(self):
-		a = {'a': 1, 'b': 2}
-		expected = {1: 'a', 2: 'b'}
-		self.assertEqual(f(a), expected)
+    def testInt(self):
+        a = {'a': 1, 'b': 2}
+        expected = {1: 'a', 2: 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testLong(self):
-		a = {'a': 1L, 'b': 2L}
-		expected = {1L: 'a', 2L: 'b'}
-		self.assertEqual(f(a), expected)
+    def testLong(self):
+        a = {'a': 1L, 'b': 2L}
+        expected = {1L: 'a', 2L: 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testFloat(self):
-		a = {'a': 1.0, 'b': 2.0}
-		expected = {1.0: 'a', 2.0: 'b'}
-		self.assertEqual(f(a), expected)
+    def testFloat(self):
+        a = {'a': 1.0, 'b': 2.0}
+        expected = {1.0: 'a', 2.0: 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testComplex(self):
-		a = {'a': 1+2j, 'b': 2+3j}
-		expected = {1+2j: 'a', 2+3j: 'b'}
-		self.assertEqual(f(a), expected)
+    def testComplex(self):
+        a = {'a': 1+2j, 'b': 2+3j}
+        expected = {1+2j: 'a', 2+3j: 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testString(self):
-		a = {'a': 'abc', 'b': 'def'}
-		expected = {'abc': 'a', 'def': 'b'}
-		self.assertEqual(f(a), expected)
+    def testString(self):
+        a = {'a': 'abc', 'b': 'def'}
+        expected = {'abc': 'a', 'def': 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testTuple(self):
-		a = {'a': (1, 2), 'b': (3, 4)}
-		expected = {(1, 2): 'a', (3, 4): 'b'}
-		self.assertEqual(f(a), expected)
+    def testTuple(self):
+        a = {'a': (1, 2), 'b': (3, 4)}
+        expected = {(1, 2): 'a', (3, 4): 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testNestedTuple(self):
-		a = {'a': (1, (2)), 'b': ((3), 4)}
-		expected = {(1, (2)): 'a', ((3), 4): 'b'}
-		self.assertEqual(f(a), expected)
+    def testNestedTuple(self):
+        a = {'a': (1, (2)), 'b': ((3), 4)}
+        expected = {(1, (2)): 'a', ((3), 4): 'b'}
+        self.assertEqual(swap_key_values(a), expected)
 
-	def testComplexTuple(self):
-		a = {'a': (1, ('abc', ()), ''), 'b': (((), ''), (3), 'def')}
-		expected = {(1, ('abc', ()), ''): 'a', (((), ''), (3), 'def'): 'b'}
-		self.assertEqual(f(a), expected)
+    def testComplexTuple(self):
+        a = {'a': (1, ('abc', ()), ''), 'b': (((), ''), (3), 'def')}
+        expected = {(1, ('abc', ()), ''): 'a', (((), ''), (3), 'def'): 'b'}
+        self.assertEqual(swap_key_values(a), expected)
+
 
 class SwapKeyValuesInvalidTests(unittest.TestCase):
-	"""Tests for invalid dictionary key types"""
+    """Tests for invalid dictionary key types"""
 
-	def testList(self):
-		a = {'a': [1, 2, 3]}
-		self.assertFalse(f(a))
+    def testList(self):
+        a = {'a': [1, 2, 3]}
+        self.assertFalse(swap_key_values(a))
 
-	def testNestedList(self):
-		a = {'a': [1, 2, [3]]}
-		self.assertFalse(f(a))
+    def testNestedList(self):
+        a = {'a': [1, 2, [3]]}
+        self.assertFalse(swap_key_values(a))
 
-	def testDictionary(self):
-		a = {'a': {'b': 1, 'c': 2}}
-		self.assertFalse(f(a))
+    def testDictionary(self):
+        a = {'a': {'b': 1, 'c': 2}}
+        self.assertFalse(swap_key_values(a))
 
-	def testNestedDictionary(self):
-		a = {'a': {'b': {'c': 1}}}
-		self.assertFalse(f(a))
+    def testNestedDictionary(self):
+        a = {'a': {'b': {'c': 1}}}
+        self.assertFalse(swap_key_values(a))
 
-	def testTupleOne(self):
-		a = {'a': (1, [2])}
-		self.assertFalse(f(a))
+    def testTupleOne(self):
+        a = {'a': (1, [2])}
+        self.assertFalse(swap_key_values(a))
 
-	def testTupleTwo(self):
-		a = {'a': (1, {'a': 2})}
-		self.assertFalse(f(a))
+    def testTupleTwo(self):
+        a = {'a': (1, {'a': 2})}
+        self.assertFalse(swap_key_values(a))
+
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
