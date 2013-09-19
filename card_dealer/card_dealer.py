39d82b5625ce64713aa29ae340ece83aabed12a1
linkyndy <linkyndy@gmail.com>
2013-09-19 15:38:26 +0300

    Refactored code for card dealer

--- card_dealer/card_dealer.py
+++ card_dealer/card_dealer.py
@@ -1,77 +1,98 @@
 import random
 
+
 class Card(object):
-	"""Handles individual cards"""
+    """Handles individual cards"""
 
-	def __init__(self, code):
-		self.code = code
+    def __init__(self, code):
+        self.code = code
 
-	def __str__(self):
-		codes = ('2','3','4','5','6','7','8','9','10','J','Q','K','A')
-		symbols = (u'\u2660', u'\u2663', u'\u2665', u'\u2666')
+    def __str__(self):
+        codes = ('2', '3', '4', '5', '6', '7', '8',
+                 '9', '10', 'J', 'Q', 'K', 'A')
+        symbols = (u'\u2660', u'\u2663', u'\u2665', u'\u2666')
 
-		return (codes[self.code%13] + symbols[self.code/13]).encode('utf-8')
+        return (codes[self.code % 13] + symbols[self.code/13]).encode('utf-8')
+
 
 class Deck(object):
-	"""Handles a deck consisting of 52 cards"""
+    """Handles a deck consisting of 52 cards"""
 
-	def __init__(self):
-		self.cards = [Card(i) for i in range(0, 52)]
+    def __init__(self):
+        self.cards = [Card(i) for i in range(0, 52)]
 
-	def __str__(self):
-		return '  '.join([str(card) for card in self.cards])
+    def __str__(self):
+        return '  '.join([str(card) for card in self.cards])
 
-	def shuffle(self):
-		random.shuffle(self.cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-	def sort(self):
-		self.cards.sort(key=lambda k: k.code)
+    def sort(self):
+        self.cards.sort(key=lambda k: k.code)
 
-	def draw(self):
-		return self.cards.pop()
+    def draw(self, n=1):
+        if n == 1:
+            return self.cards.pop()
+
+        return [self.cards.pop() for i in range(0, n)]
+
 
 class Hand(object):
-	"""Handles a hand consisting of 2 cards"""
+    """Handles a hand consisting of 2 cards"""
 
-	def __init__(self, deck):
-		self.cards = [deck.draw(), deck.draw()]
+    def __init__(self, deck):
+        self.cards = deck.draw(2)
 
-	def __str__(self):
-		return '  '.join([str(card) for card in self.cards])
+    def __str__(self):
+        return '  '.join([str(card) for card in self.cards])
 
-class Player(object):
-	"""Handles players involved in the game"""
 
-	def __init__(self, name='unknown'):
-		self.name = name
+class Person(object):
+    """Handles persons involved in the game"""
 
-	def __str__(self):
-		return self.name
+    def __init__(self, name='unknown'):
+        self.name = name
 
-class Dealer(Player):
-	"""Handles the dealer of the game"""
+    def __str__(self):
+        return self.name
 
-	def deal(self, deck, players):
-		"""Deals the cards to the players and on the table"""
 
-		return {
-			'players': [{str(p): Hand(deck)} for p in players], 
-			'table': [deck.draw() for i in range(0, 5)]
-		}
+class Player(Person):
+    """Handles players involved in the game"""
+
+    pass
+
+
+class Dealer(Person):
+    """Handles the dealer of the game"""
+
+    def deal_to_players(self, deck, players):
+        """Deals the cards to the players"""
+
+        return {str(player): Hand(deck) for player in players}
+
+    def deal_on_table(self, deck):
+        """Deals the cards on the table"""
+
+        return deck.draw(5)
+
 
 class Game(object):
-	"""Handles the poker game itself"""
+    """Handles the poker game itself"""
 
-	def __init__(self, dealer, players):
-		self.dealer = dealer
-		self.players = players
+    def __init__(self, dealer, players):
+        self.dealer = dealer
+        self.players = players
 
-	def play(self, times=1):
-		"""Plays a game consisting of several deal times"""
+    def play(self, times=1):
+        """Plays a game consisting of several deal times"""
 
-		game = []
+        game = []
 
-		for t in range(0, times):
-			game.append(self.dealer.deal(Deck(), self.players))
+        for t in range(0, times):
+            self.deck = Deck()
+            game.append({'players': self.dealer.deal_to_players(self.deck,
+                                                                self.players),
+                         'table': self.dealer.deal_on_table(self.deck)})
 
-		return game
+        return game
--- card_dealer/tests.py
+++ card_dealer/tests.py
@@ -3,102 +3,106 @@ import unittest
 
 
 class CardTests(unittest.TestCase):
-	"""Tests cards"""
+    """Tests cards"""
 
-	def testCardOutput(self):
-		c = Card(14)
-		self.assertEqual(str(c), u'3\u2663'.encode('utf-8'))
+    def testCardOutput(self):
+        c = Card(14)
+        self.assertEqual(str(c), u'3\u2663'.encode('utf-8'))
+
 
 class DeckTests(unittest.TestCase):
-	"""Tests decks"""
+    """Tests decks"""
 
-	def setUp(self):
-		self.deck = Deck()
+    def setUp(self):
+        self.deck = Deck()
 
-	def testNumberOfCards(self):
-		self.assertEqual(len(self.deck.cards), 52)
+    def testNumberOfCards(self):
+        self.assertEqual(len(self.deck.cards), 52)
 
-	def testShuffle(self):
-		shuffled = []
+    def testShuffle(self):
+        shuffled = []
 
-		for i in range(0, 100):
-			shuffled.append([c.code for c in self.deck.cards])
-			self.deck.shuffle()
-			self.assertNotIn([c.code for c in self.deck.cards], shuffled)
+        for i in range(0, 100):
+            shuffled.append([c.code for c in self.deck.cards])
+            self.deck.shuffle()
+            self.assertNotIn([c.code for c in self.deck.cards], shuffled)
 
-	def testSortWithoutShuffle(self):
-		self.deck.sort()
-		self.assertEqual([c.code for c in self.deck.cards], range(0, 52))
+    def testSortWithoutShuffle(self):
+        self.deck.sort()
+        self.assertEqual([c.code for c in self.deck.cards], range(0, 52))
 
-	def testSortWithShuffle(self):
-		self.deck.shuffle()
-		self.deck.sort()
-		self.assertEqual([c.code for c in self.deck.cards], range(0, 52))
+    def testSortWithShuffle(self):
+        self.deck.shuffle()
+        self.deck.sort()
+        self.assertEqual([c.code for c in self.deck.cards], range(0, 52))
 
-	def testDrawNumberOfCardsLeft(self):
-		self.deck.draw()
-		self.assertEqual(len(self.deck.cards), 51)
+    def testDrawNumberOfCardsLeft(self):
+        self.deck.draw()
+        self.assertEqual(len(self.deck.cards), 51)
 
-	def testDrawCardsLeft(self):
-		card = self.deck.draw()
-		self.assertNotIn(card.code, [c.code for c in self.deck.cards])
+    def testDrawCardsLeft(self):
+        card = self.deck.draw()
+        self.assertNotIn(card.code, [c.code for c in self.deck.cards])
+
 
 class HandTests(unittest.TestCase):
-	"""Hand tests"""
+    """Hand tests"""
 
-	def setUp(self):
-		self.hand = Hand(Deck())
+    def setUp(self):
+        self.hand = Hand(Deck())
 
-	def testNumberOfCards(self):
-		self.assertEqual(len(self.hand.cards), 2)
+    def testNumberOfCards(self):
+        self.assertEqual(len(self.hand.cards), 2)
+
 
 class PlayerTests(unittest.TestCase):
-	"""Tests players"""
+    """Tests players"""
 
-	def testPlayerName(self):
-		p = Player('John')
-		self.assertEqual(str(p), 'John')
+    def testPlayerName(self):
+        p = Player('John')
+        self.assertEqual(str(p), 'John')
+
 
 class DealerTests(unittest.TestCase):
-	"""Tests dealers"""
+    """Tests dealers"""
 
-	def setUp(self):
-		self.dealer = Dealer('Jane')
+    def setUp(self):
+        self.dealer = Dealer('Jane')
 
-	def testDealerName(self):
-		self.assertEqual(str(self.dealer), 'Jane')
+    def testDealerName(self):
+        self.assertEqual(str(self.dealer), 'Jane')
 
-	def testDealNumberOfPlayers(self):
-		deal = self.dealer.deal(Deck(), [Player(), Player()])
-		self.assertEqual(len(deal['players']), 2)
+    def testDealNumberOfPlayers(self):
+        deal = self.dealer.deal_to_players(Deck(), [Player('a'), Player('b')])
+        self.assertEqual(len(deal), 2)
 
-	def testDealPlayerNames(self):
-		deal = self.dealer.deal(Deck(), [Player('a'), Player('b')])
-		self.assertTrue('a' in deal['players'][0] or 'b' in deal['players'][0])
-		self.assertTrue('a' in deal['players'][1] or 'b' in deal['players'][1])
+    def testDealPlayerNames(self):
+        deal = self.dealer.deal_to_players(Deck(), [Player('a'), Player('b')])
+        self.assertTrue('a' in deal and 'b' in deal)
 
-	def testDealNumberOnTable(self):
-		deal = self.dealer.deal(Deck(), [Player(), Player()])
-		self.assertEqual(len(deal['table']), 5)
+    def testDealNumberOnTable(self):
+        deal = self.dealer.deal_on_table(Deck())
+        self.assertEqual(len(deal), 5)
+
 
 class GameTests(unittest.TestCase):
-	"""Tests games"""
+    """Tests games"""
 
-	def setUp(self):
-		self.game = Game(Dealer(), [Player(), Player()])
+    def setUp(self):
+        self.game = Game(Dealer(), [Player(), Player()])
 
-	def testPlayNone(self):
-		self.assertEqual(self.game.play(0), [])
+    def testPlayNone(self):
+        self.assertEqual(self.game.play(0), [])
 
-	def testPlayDefault(self):
-		self.assertEqual(len(self.game.play()), 1)
+    def testPlayDefault(self):
+        self.assertEqual(len(self.game.play()), 1)
 
-	def testPlayRandom(self):
-		self.assertEqual(len(self.game.play(165)), 165)
+    def testPlayRandom(self):
+        self.assertEqual(len(self.game.play(165)), 165)
 
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
