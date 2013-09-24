04c0558661486719d659d3b8c22b092106678a35
linkyndy <linkyndy@gmail.com>
2013-09-24 21:05:06 +0300

    Refactored code and made it compliant with PEP8

--- model_implementation/model_implementation.py
+++ model_implementation/model_implementation.py
@@ -1,84 +1,84 @@
 class ModelBase(type):
-	"""Base type for created models"""
+    """Base type for created models"""
 
-	def __new__(cls, name, bases, attrs):
-		super_new = super(ModelBase, cls).__new__
+    def __new__(cls, name, bases, attrs):
+        super_new = super(ModelBase, cls).__new__
 
-		# Don't add fields for base class Model
-		if name == 'Model':
-			return super_new(cls, name, bases, attrs)
+        # Don't add fields for base class Model
+        if name == 'Model':
+            return super_new(cls, name, bases, attrs)
 
-		module = attrs['__module__']
-		new_class = super_new(cls, name, bases, {'__module__': module})
+        module = attrs['__module__']
+        new_class = super_new(cls, name, bases, {'__module__': module})
 
-		# Initialize fields dictionary on new class
-		setattr(new_class, 'fields', {})
+        # Initialize fields dictionary on new class
+        setattr(new_class, 'fields', {})
 
-		# Add attrs to new class' fields dictionary
-		# Only add attrs declared as fields
-		for attr, val in attrs.iteritems():
-			if isinstance(val, Field):
-				new_class.add_to_class(attr, val)
-		
-		return new_class
+        # Add attrs to new class' fields dictionary
+        # Only add attrs declared as fields
+        for attr, val in attrs.iteritems():
+            if isinstance(val, Field):
+                new_class.add_to_class(attr, val)
 
-	def add_to_class(cls, attr, val):
-		"""Add an attribute to class' fields dictionary"""
-		
-		getattr(cls, 'fields')[attr] = val
+        return new_class
+
+    def add_to_class(cls, attr, val):
+        """Add an attribute to class' fields dictionary"""
+
+        getattr(cls, 'fields')[attr] = val
 
 
 class Model(object):
-	"""Base class for created objects"""
+    """Base class for created objects"""
 
-	__metaclass__ = ModelBase
+    __metaclass__ = ModelBase
 
-	def __init__(self, **kwargs):
-		# Check the supplied number of kwargs
-		if len(kwargs) > len(self.fields):
-			raise IndexError('Too many arguments supplied to model')
+    def __init__(self, **kwargs):
+        # Check the supplied number of kwargs
+        if len(kwargs) > len(self.fields):
+            raise IndexError('Too many arguments supplied to model')
 
-		# For each declared model field attach supplied kwarg
-		# or default field value
-		# Perform validation also
-		for field, field_type in self.fields.iteritems():
-			val = kwargs.pop(field, field_type.get_default_value())
-			setattr(self, field, field_type.validate(val))
+        # For each declared model field attach supplied kwarg
+        # or default field value
+        # Perform validation also
+        for field, field_type in self.fields.iteritems():
+            val = kwargs.pop(field, field_type.get_default_value())
+            setattr(self, field, field_type.validate(val))
 
 
 class Field(object):
-	def __init__(self, max_length=None):
-		self.max_length = max_length
+    def __init__(self, max_length=None):
+        self.max_length = max_length
 
-	def validate(self, value):
-		"""Should be overridden by subclasses to provide validation"""
+    def validate(self, value):
+        """Should be overridden by subclasses to provide validation"""
 
-		pass
+        pass
 
 
 class IntegerField(Field):
-	def __init__(self, *args, **kwargs):
-		super(IntegerField, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(IntegerField, self).__init__(*args, **kwargs)
 
-	def get_default_value(self):
-		return 0
+    def get_default_value(self):
+        return 0
 
-	def validate(self, val):
-		try:
-			return int(val)
-		except:
-			raise AttributeError('Invalid value for IntegerField')
+    def validate(self, val):
+        try:
+            return int(val)
+        except:
+            raise AttributeError('Invalid value for IntegerField')
 
 
 class StringField(Field):
-	def __init__(self, *args, **kwargs):
-		super(StringField, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(StringField, self).__init__(*args, **kwargs)
 
-	def get_default_value(self):
-		return ''
+    def get_default_value(self):
+        return ''
 
-	def validate(self, val):
-		try:
-			return str(val)
-		except:
-			raise AttributeError('Invalid value for StringField')
+    def validate(self, val):
+        try:
+            return str(val)
+        except:
+            raise AttributeError('Invalid value for StringField')
--- model_implementation/tests.py
+++ model_implementation/tests.py
@@ -1,96 +1,103 @@
 from model_implementation import ModelBase, Model, Field, StringField, IntegerField
 import unittest
 
+
 class ModelBaseTests(unittest.TestCase):
-	"""Various model base tests"""
+    """Various model base tests"""
 
-	def setUp(self):
-		class TestModel(Model):
-			int_field = IntegerField()
-			string_field = StringField()
-		
-		self.test_model = TestModel
+    def setUp(self):
+        class TestModel(Model):
+            int_field = IntegerField()
+            string_field = StringField()
 
-	def testFieldsDictPresent(self):
-		self.assertTrue(hasattr(self.test_model, 'fields'))
+        self.test_model = TestModel
 
-	def testAllFieldsPresent(self):
-		self.assertEqual(len(self.test_model.fields), 2)
-		self.assertIn('int_field', self.test_model.fields)
-		self.assertIn('string_field', self.test_model.fields)
+    def testFieldsDictPresent(self):
+        self.assertTrue(hasattr(self.test_model, 'fields'))
+
+    def testAllFieldsPresent(self):
+        self.assertEqual(len(self.test_model.fields), 2)
+        self.assertIn('int_field', self.test_model.fields)
+        self.assertIn('string_field', self.test_model.fields)
+
 
 class ModelTests(unittest.TestCase):
-	"""Various model tests"""
+    """Various model tests"""
 
-	def setUp(self):
-		class TestModel(Model):
-			int_field = IntegerField()
-			string_field = StringField()
-		
-		self.test_model = TestModel
+    def setUp(self):
+        class TestModel(Model):
+            int_field = IntegerField()
+            string_field = StringField()
 
-	def testKwargsSet(self):
-		test_object = self.test_model(string_field='test', int_field=1)
-		self.assertTrue(hasattr(test_object, 'string_field'))
-		self.assertTrue(hasattr(test_object, 'int_field'))
-		self.assertEqual(test_object.string_field, 'test')
-		self.assertEqual(test_object.int_field, 1)
+        self.test_model = TestModel
 
-	def testTooManyKwargs(self):
-		self.assertRaises(IndexError, self.test_model, string_field='test', int_field=1, extra=2)
+    def testKwargsSet(self):
+        test_object = self.test_model(string_field='test', int_field=1)
+        self.assertTrue(hasattr(test_object, 'string_field'))
+        self.assertTrue(hasattr(test_object, 'int_field'))
+        self.assertEqual(test_object.string_field, 'test')
+        self.assertEqual(test_object.int_field, 1)
 
-	def testDefaultValueUse(self):
-		test_object = self.test_model()
-		self.assertEqual(test_object.string_field, '')
-		self.assertEqual(test_object.int_field, 0)
+    def testTooManyKwargs(self):
+        self.assertRaises(IndexError,
+                          self.test_model,
+                          string_field='test', int_field=1, extra=2)
+
+    def testDefaultValueUse(self):
+        test_object = self.test_model()
+        self.assertEqual(test_object.string_field, '')
+        self.assertEqual(test_object.int_field, 0)
+
 
 class FieldTests(unittest.TestCase):
-	"""Various model tests"""
+    """Various model tests"""
 
-	def testNoMaxLength(self):
-		field = Field()
-		self.assertEqual(field.max_length, None)
+    def testNoMaxLength(self):
+        field = Field()
+        self.assertEqual(field.max_length, None)
 
-	def testSpecifiedMaxLength(self):
-		field = Field(max_length=50)
-		self.assertEqual(field.max_length, 50) 
+    def testSpecifiedMaxLength(self):
+        field = Field(max_length=50)
+        self.assertEqual(field.max_length, 50)
+
 
 class IntegerFieldTests(unittest.TestCase):
-	"""Various integer field tests"""
+    """Various integer field tests"""
 
-	def setUp(self):
-		self.integer_field = IntegerField()
+    def setUp(self):
+        self.integer_field = IntegerField()
 
-	def testDefaultValue(self):
-		self.assertEqual(self.integer_field.get_default_value(), 0)
+    def testDefaultValue(self):
+        self.assertEqual(self.integer_field.get_default_value(), 0)
 
-	def testValidateInt(self):
-		self.assertEqual(self.integer_field.validate(10), 10)
+    def testValidateInt(self):
+        self.assertEqual(self.integer_field.validate(10), 10)
 
-	def testValidateCastInt(self):
-		self.assertEqual(self.integer_field.validate('10'), 10)
+    def testValidateCastInt(self):
+        self.assertEqual(self.integer_field.validate('10'), 10)
 
-	def testValidateOther(self):
-		self.assertRaises(AttributeError, self.integer_field.validate, 'string')
+    def testValidateOther(self):
+        self.assertRaises(AttributeError, self.integer_field.validate, 'str')
+
 
 class StringFieldTests(unittest.TestCase):
-	"""Various string field tests"""
+    """Various string field tests"""
 
-	def setUp(self):
-		self.string_field = StringField()
+    def setUp(self):
+        self.string_field = StringField()
 
-	def testDefaultValue(self):
-		self.assertEqual(self.string_field.get_default_value(), '')
+    def testDefaultValue(self):
+        self.assertEqual(self.string_field.get_default_value(), '')
 
-	def testValidateString(self):
-		self.assertEqual(self.string_field.validate('string'), 'string')
+    def testValidateString(self):
+        self.assertEqual(self.string_field.validate('string'), 'string')
 
-	def testValidateCastInt(self):
-		self.assertEqual(self.string_field.validate(10), '10')
+    def testValidateCastInt(self):
+        self.assertEqual(self.string_field.validate(10), '10')
 
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
