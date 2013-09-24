b3292e2fcd3a72bfa7b7e2ab9aaf1643cdf0e987
linkyndy <linkyndy@gmail.com>
2013-09-24 20:57:31 +0300

    Finished writing tests for model implementation

--- model_implementation/tests.py
+++ model_implementation/tests.py
@@ -2,31 +2,47 @@ from model_implementation import ModelBase, Model, Field, StringField, IntegerFi
 import unittest
 
 class ModelBaseTests(unittest.TestCase):
-	"""Various model tests"""
+	"""Various model base tests"""
 
 	def setUp(self):
-		class TestModel(object):
-			__metaclass__ = ModelBase
+		class TestModel(Model):
 			int_field = IntegerField()
+			string_field = StringField()
 		
 		self.test_model = TestModel
-
-		class Model(object):
-			__metaclass__ = ModelBase
-
-		self.model = Model
 
 	def testFieldsDictPresent(self):
 		self.assertTrue(hasattr(self.test_model, 'fields'))
 
-	def testFieldsDictNotPresent(self):
-		self.assertFalse(hasattr(self.model, 'fields'))
+	def testAllFieldsPresent(self):
+		self.assertEqual(len(self.test_model.fields), 2)
+		self.assertIn('int_field', self.test_model.fields)
+		self.assertIn('string_field', self.test_model.fields)
 
 class ModelTests(unittest.TestCase):
 	"""Various model tests"""
 
-	pass 
+	def setUp(self):
+		class TestModel(Model):
+			int_field = IntegerField()
+			string_field = StringField()
+		
+		self.test_model = TestModel
 
+	def testKwargsSet(self):
+		test_object = self.test_model(string_field='test', int_field=1)
+		self.assertTrue(hasattr(test_object, 'string_field'))
+		self.assertTrue(hasattr(test_object, 'int_field'))
+		self.assertEqual(test_object.string_field, 'test')
+		self.assertEqual(test_object.int_field, 1)
+
+	def testTooManyKwargs(self):
+		self.assertRaises(IndexError, self.test_model, string_field='test', int_field=1, extra=2)
+
+	def testDefaultValueUse(self):
+		test_object = self.test_model()
+		self.assertEqual(test_object.string_field, '')
+		self.assertEqual(test_object.int_field, 0)
 
 class FieldTests(unittest.TestCase):
 	"""Various model tests"""
