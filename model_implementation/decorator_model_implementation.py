f24219341e16b000c3f33a5cbaf4d9f6040f2afc
linkyndy <linkyndy@gmail.com>
2013-09-26 17:57:32 +0300

    Added InvalidField exception

--- model_implementation/decorator_model_implementation.py
+++ model_implementation/decorator_model_implementation.py
@@ -1,6 +1,10 @@
 import decorator_field_types
 
 
+class InvalidField(Exception):
+    pass
+
+
 def modellize(func):
     """Decorator which makes any function act like a model"""
 
@@ -11,7 +15,10 @@ def modellize(func):
 
         fields = {}
         for field, field_type in func().iteritems():
-            field_func = getattr(decorator_field_types, field_type)
+            try:
+                field_func = getattr(decorator_field_types, field_type)
+            except AttributeError:
+                raise InvalidField('Field type \'%s\' is invalid' % field_type)
             fields[field] = field_func(kwargs[field]) if field in kwargs else field_func()
         return fields
     return process
